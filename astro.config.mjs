8f0f2b4b98a242faac80dd83b302d960e9a55e45
Danny Sullivan <dsully15@gmail.com>
2025-08-26 13:53:57 -0400

    initial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://karinrabinstherapy.com', 
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'auto',
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  }
+});
